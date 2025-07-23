5ad7e9eadcea586d14c81bc3420b81dd0861d540
AppaiahKelowna <appaiahtwokelowna@gmail.com>
2025-07-22 18:53:33 -0700

    Initial firebase and NativeWind setup

--- App.tsx
+++ App.tsx
@@ -6,23 +6,22 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View className="flex-1 justify-center items-center">
+      <Text> Home Screen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
